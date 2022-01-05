153f1a73f21216a2fc6b5533038225358ed4e32e
Cornel Marck <cornel@cornelmarck.com>
2022-01-05 22:54:02 +0100

    create collinear-points project

---
+++ collinear-points/src/BruteCollinearPoints.java
@@ -0,0 +1,15 @@
+public class BruteCollinearPoints {
+    // finds all line segments containing 4 points. Assume that there are no line segments longer than 4.
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+    // the number of line segments
+    public int numberOfSegments() {
+
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+
+    }
+}
---
+++ collinear-points/src/CollinearSegment.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollinearSegment {
+    private List<Point> points;
+
+    public CollinearSegment() {
+
+    }
+
+    public LineSegment getLineSegment() {
+        
+    }
+
+
+
+}
